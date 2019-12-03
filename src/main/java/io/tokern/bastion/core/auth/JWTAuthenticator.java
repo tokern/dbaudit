d8f675c3c44c32a97dd6d403db74e2f26300a473
Rajat Venkatesh <vrajat@dblint.io>
2019-12-03 21:59:43 +0530

    fix: Fix AuthFilter to consider tokens in header.

--- src/main/java/io/tokern/bastion/core/auth/JWTAuthenticator.java
+++ src/main/java/io/tokern/bastion/core/auth/JWTAuthenticator.java
@@ -1,5 +1,7 @@
 package io.tokern.bastion.core.auth;
 
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import io.dropwizard.auth.AuthenticationException;
 import io.dropwizard.auth.Authenticator;
 import io.tokern.bastion.api.User;
@@ -7,23 +9,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import javax.ws.rs.core.Cookie;
 import java.util.Optional;
 
-public class JWTAuthenticator implements Authenticator<Cookie, User> {
+public class JWTAuthenticator implements Authenticator<DecodedJWT, User> {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(JWTAuthenticator.class);
 
-  private final JwtTokenManager jwtTokenManager;
-
   @Inject
-  public JWTAuthenticator(final JwtTokenManager jwtTokenManager) {
-    this.jwtTokenManager = jwtTokenManager;
-  }
+  public JWTAuthenticator() {}
 
   @Override
-  public Optional<User> authenticate(final Cookie cookie) throws AuthenticationException {
-    final String token = cookie.getValue();
-    return Optional.of(jwtTokenManager.verifyToken(token));
+  public Optional<User> authenticate(final DecodedJWT jwt) throws AuthenticationException {
+    Claim id = jwt.getClaim("id");
+    Claim email = jwt.getClaim("email");
+    Claim name = jwt.getClaim("name");
+    Claim orgId = jwt.getClaim("orgId");
+
+    return Optional.of(new User(id.asInt(), name.asString(), email.asString(), orgId.asInt()));
   }
 }
