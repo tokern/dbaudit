da9d61a5c775854f3c6091fbba496eb9d66c3b78
Rajat Venkatesh <vrajat@dblint.io>
2019-12-04 11:25:14 +0530

    dev: Clean up AuthFilter to use correct components

--- src/main/java/io/tokern/bastion/core/auth/JWTAuthenticator.java
+++ src/main/java/io/tokern/bastion/core/auth/JWTAuthenticator.java
@@ -1,5 +1,8 @@
 package io.tokern.bastion.core.auth;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import io.dropwizard.auth.AuthenticationException;
@@ -11,19 +14,37 @@ import org.slf4j.LoggerFactory;
 import javax.inject.Inject;
 import java.util.Optional;
 
-public class JWTAuthenticator implements Authenticator<DecodedJWT, User> {
+public class JWTAuthenticator implements Authenticator<String, User> {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(JWTAuthenticator.class);
 
+  private final Algorithm algorithm;
+  private final JWTVerifier jwtVerifier;
+  private final String issuer = "bastion";
+
   @Inject
-  public JWTAuthenticator() {}
+  public JWTAuthenticator(JWTConfiguration configuration) {
+    algorithm = Algorithm.HMAC256(configuration.getJwtSecret());
+
+    jwtVerifier = JWT.require(algorithm)
+        .withIssuer(issuer)
+        .build();
+  }
 
   @Override
-  public Optional<User> authenticate(final DecodedJWT jwt) throws AuthenticationException {
+  public Optional<User> authenticate(String token) throws AuthenticationException {
+    DecodedJWT jwt = jwtVerifier.verify(token);
+    LOGGER.info("Decoded JWT token");
+
     Claim id = jwt.getClaim("id");
     Claim email = jwt.getClaim("email");
     Claim name = jwt.getClaim("name");
     Claim orgId = jwt.getClaim("orgId");
+
+    if (id == null || email == null || name == null || orgId == null) {
+      LOGGER.warn("Failed JWT token verification");
+      return Optional.empty();
+    }
 
     return Optional.of(new User(id.asInt(), name.asString(), email.asString(), orgId.asInt()));
   }
